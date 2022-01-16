1ef4bbf63bdf9237e39723b8360119638277026b
Kyle Goodwin <kyle.goodwin93@gmail.com>
2022-01-16 18:43:28 -0500

    commit tree 75c7859201e8d04e1966913bcc52836c2d566f1f

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,12 +1,17 @@
 import React from "react";
 
-function Stock() {
+function Stock({stock, onClick}) {
+
+  const {name, price, ticker} = stock
+
   return (
     <div>
-      <div className="card">
+      <div 
+      onClick={onClick}
+      className="card">
         <div className="card-body">
-          <h5 className="card-title">{"Compant Name"}</h5>
-          <p className="card-text">{"Stock Price"}</p>
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">{ticker}: {price}</p>
         </div>
       </div>
     </div>
